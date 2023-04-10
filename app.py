9bb2d77e494e540c144e16c1410f7bbb371afb61
manik-18 <guptamanik1810@gmail.com>
2023-04-10 21:22:50 +0530

    third Commit

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
+import sys
+from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 from flask import Flask, render_template, request
 import tensorflow as tf
+import concurrent.futures
 import reportMail
+import pyttsx3
 
 new_model = tf.keras.models.load_model('trained_model.hdf5')
 app = Flask(__name__)
 predictions = ['MildDemented', 'ModerateDemented', 'NonDemented', 'VeryMildDemented']
+
+def typing(text):
+    for char in text:
+        sleep(0.04)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+def textToSpeech(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 220)
+    engine.say(text)
+    engine.runAndWait()
+    del engine
+
+def parallel(text):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        future_tasks = {executor.submit(
+            textToSpeech, text), executor.submit(typing, text)}
+        for future in concurrent.futures.as_completed(future_tasks):
+            try:
+                data = future.result()
+            except Exception as e:
+                print(e)
 
 def predict_new(path):
     img = cv2.imread(path)
@@ -36,6 +65,7 @@ def predict():
     imagefile.save(image_path)
     predicted_value = predict_new(image_path)
     print(name,age, emailId,contact,symptoms,predicted_value)
+    parallel(f"Your stage is {predicted_value}")
     reportMail.send(name,age,emailId,contact,symptoms,predicted_value)
     return render_template('index.html', name=name, emailId=emailId, predicted_value=predicted_value)
 
--- reportMail.py
+++ reportMail.py
@@ -2,35 +2,105 @@ import random
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.colors import HexColor
 from reportlab.lib.units import inch
+from reportlab.lib.colors import black
 from reportlab.pdfgen import canvas
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import uploadGoogleCloud
+import datetime
 
 
-def send(name,age,emailId,contact,symptoms,predicted_value):
-    num=random.randint(100000000,999999999)
+def send(name, age, emailId, contact, symptoms, predicted_value):
+    num = random.randint(100000000, 999999999)
+    current_time = datetime.datetime.now()
     # create PDF canvas
     c = canvas.Canvas("medical_report.pdf", pagesize=letter)
+    c.setStrokeColor(black)
+    c.rect(0.5*inch, 0.5*inch, letter[0]-1*inch, letter[1]-1*inch)
+
     # add hospital image on the top left corner
-    c.drawImage("static\images\logoo.png", 20, 650, width=1.5*inch, height=1.5*inch)
+    c.drawImage("static\images\gfglogo.png", 60, 650,
+                width=2*inch, height=1*inch)
     # add report details
-    c.drawString(1*inch, 600, f"Report ID: {num}")
-    c.drawString(1*inch, 575, f"Name: {name}")
-    c.drawString(1*inch, 525, f"Age: {age}")
-    c.drawString(1*inch, 500, f"Phone No.: {contact}")
-    c.drawString(1*inch, 475, f"Email: {emailId}")
-    c.drawString(1*inch, 425, f"Symptoms: {symptoms}")
+    c.drawString(4*inch, 500, f"Date/Time: {current_time}")
+    c.drawString(1*inch, 500, f"Report ID: {num}")
+    c.drawString(1*inch, 475, f"Name: {name}")
+    c.drawString(1*inch, 450, f"Age: {age}")
+    c.drawString(1*inch, 425, f"Phone No.: {contact}")
+    c.drawString(1*inch, 400, f"Email: {emailId}")
+    c.drawString(1*inch, 375, f"Symptoms: {symptoms}")
     # add medical report result
-    c.drawString(1*inch, 400, f"Medical Report Result: {predicted_value}")
-    # Set the font color to red
-    c.setFillColor(HexColor("#FF0000"))
-    # Set the font to bold and italic
-    c.setFont("Helvetica-BoldOblique", 30)
-    # add hospital name
-    c.drawString(2*inch, 700, "ALZEHIMERS'S REPORT")
+    c.drawString(
+        1*inch, 350, f"Medical Report Result: Patient is identified to be {predicted_value}")
+    if predicted_value == 'MildDemented':
+        text = """Further advice to be followed:
+
+        1. Patient is in an early stage of Alzheimer's.
+        2. However, with proper treatment and care, the advent of the disease can be slowed.
+        3. We advise you to visit your primary care provider at the earliest to begin treatment.
+        4. Alzheimer's is a disease that gets worse as time progresses. We strongly recommend 
+           that the patient and the family take serious steps to combat it."""
+
+    elif predicted_value == 'VeryMildDemented':
+        text = """Further advice to be followed :
+
+        1. Patient is in the very early stage of Alzheimer's.
+        2. However, with proper treatment and care, the advent of the disease can be slowed.
+        3. We recommend that you visit your primary care provider at the earliest to begin 
+           treatment."""
+    elif predicted_value == 'ModerateDemented':
+        text = """Further advice to be followed :
+
+        1. Patient is in a serious stage of Alzheimer's.
+        2. Immediate treatment should begin at the earliest.
+        3. We advise you to visit your primary care provider as soon as possible.
+        4. Alzheimer's is a disease that gets worse as time progresses. We strongly 
+           recommend that the patient and the family take serious steps to combat it."""
+    elif predicted_value == 'NonDemented':
+        text = """Further advice to be followed :
+
+        1. Patient is not suffering from Alzheimer's yet.
+        2. However, we recommend making healthy lifestyle choices to lower your risk 
+           of Alzheimer's:
+        - Prevent and manage high blood pressure levels
+        - Manage blood sugar levels
+        - Maintain a healthy weight
+        - Be physically active
+        - Get enough sleep"""
+
+    # Create a text object
+    textobject = c.beginText()
+
+    # Set the position for the text object
+    textobject.setTextOrigin(1*inch, 325)
+
+    # Set the font and font size
+    textobject.setFont("Helvetica", 12)
+
+    # Add the text with multiple lines using the textLines() method
+    textobject.textLines(text)
+
+    # Draw the text on the canvas
+    c.drawText(textobject)
+
+    # Set the font color to green
+    # c.setFillColor(HexColor("#006400"))
+    # # Set the font to bold and italic
+    # c.setFont("Helvetica-Bold", 40)
+    # # add hospital name
+    # c.drawString(3*inch, 650, "CEREBRAL AI")
+    c.setFillColor(HexColor("#006400"))
+    c.setFont("Helvetica", 10)
+    c.drawString(
+        1*inch, 100, "This report will be sent to the email ID mentioned by the patient. A copy will be also be available in")
+    c.drawString(
+        1*inch, 80, "our Google Cloud, for any future references needed by the patient.")
+    # c.setFont("Helvetica", 15)
+    # c.drawString(3.1*inch, 630, "Delivering your hopes .......")
+    c.setFont("Helvetica-BoldOblique", 35)
+    c.drawString(2.1*inch, 580, "Alzheimer's report")
     # save PDF file
     c.save()
 
@@ -48,21 +118,22 @@ def send(name,age,emailId,contact,symptoms,predicted_value):
     msg.attach(MIMEText(body, 'plain'))
 
     with open("medical_report.pdf", 'rb') as f:
-        attach = MIMEApplication(f.read(),_subtype = "pdf")
-        attach.add_header('Content-Disposition','attachment',filename="medical_report.pdf")
+        attach = MIMEApplication(f.read(), _subtype="pdf")
+        attach.add_header('Content-Disposition', 'attachment',
+                          filename="medical_report.pdf")
         msg.attach(attach)
 
-    # SMTP server login
-    username = "1NH20CS124@gmail.com"
-    password = "xphclqhvzzihmrio"
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(username, password)
+        # SMTP server login
+        username = "1NH20CS124@gmail.com"
+        password = "xphclqhvzzihmrio"
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(username, password)
 
-    # send email
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+        # send email
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
 
-    # close SMTP server
-    server.quit()
-    uploadGoogleCloud.uploadtocloud()
+        # close SMTP server
+        server.quit()
+        uploadGoogleCloud.uploadtocloud()
